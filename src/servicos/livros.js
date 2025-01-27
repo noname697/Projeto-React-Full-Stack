51a415d7424eab808aaf95af3d34bdffd81cc292
Arthur Manenti <arthur.manenti.videira@gmail.com>
2025-01-27 15:36:16 -0300

    teste fabrica

---
+++ src/servicos/livros.js
@@ -0,0 +1,11 @@
+import axios from 'axios'
+
+const livrosAPI = axios.create({ baseURL: "http://localhost:8000/livros" })
+
+function getLivros() {
+    const response = livrosAPI.get('/')
+
+    return response.data
+}
+
+export { getLivros }
