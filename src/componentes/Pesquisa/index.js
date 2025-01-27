6e4e3daecdb46da424a0a420b26fafd5593ac092
Arthur Manenti <arthur.manenti.videira@gmail.com>
2025-01-27 16:05:23 -0300

    Conexao front e back

--- src/componentes/Pesquisa/index.js
+++ src/componentes/Pesquisa/index.js
@@ -1,7 +1,8 @@
 import Input from '../Input'
 import styled from 'styled-components'
-import { useState } from 'react'
-import { livros } from './dadosPesquisa'
+import { useState, useEffect } from 'react'
+import { getLivros } from '../../servicos/livros.js'
+
 
 const PesquisaContainer = styled.section`
     background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
@@ -47,6 +48,16 @@ const Resultado = styled.div`
 
 function Pesquisa() {
     const [livrosPesquisados, setLivrosPesquisados] = useState([])
+    const [livros, setLivros] = useState([])
+
+    useEffect(() => {
+        fetchLivros()
+    }, [livros])
+
+    async function fetchLivros() {
+        const livrosDaAPI = await getLivros()
+        setLivros(livrosDaAPI)
+    }
 
     return (
         <PesquisaContainer>
@@ -56,16 +67,16 @@ function Pesquisa() {
                 placeholder="Escreva sua próxima leitura"
                 onBlur={evento => {
                     const textoDigitado = evento.target.value
-                    const resultadoPesquisa = livros.filter( livro => livro.nome.includes(textoDigitado))
+                    const resultadoPesquisa = livros.filter(livro => livro.nome.includes(textoDigitado))
                     setLivrosPesquisados(resultadoPesquisa)
                 }}
             />
-            { livrosPesquisados.map( livro => (
+            {livrosPesquisados.map(livro => (
                 <Resultado>
-                    <img src={livro.src}/>
+                    <img src={livro.src} alt='' />
                     <p>{livro.nome}</p>
                 </Resultado>
-            ) ) }
+            ))}
         </PesquisaContainer>
     )
 }
